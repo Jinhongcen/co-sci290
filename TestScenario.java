f9362d91c569ed1b2dcb689db9dd70c3ff1651ee
jinhongcen1773 <jinhongcen1773@gmail.com>
2018-02-20 18:29:38 -0500

    test

--- TestScenario.java
+++ TestScenario.java
@@ -14,8 +14,14 @@ public class TestScenario{
         else if(randomNum <=80 && randomNum >=70){
           System.out.println("You get a Biscuits.");
         }
+      else if(randomNum <=60 && randomNum >=50){
+          System.out.println("You get a water.");
+        }
+      else if(randomNum <=40 && randomNum >=30){
+          System.out.println("You get a bug.");
+        }
         else{
-          System.out.println("You're hungry.");
+          System.out.println("You're hungry,gonna die,go to find something eat.");
         }
       
       }
