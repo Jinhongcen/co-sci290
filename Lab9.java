33cce1b482864b3bd608fed20d4addd67e81fce2
jinhongcen1773 <jinhongcen1773@gmail.com>
2018-03-15 18:29:31 -0400

    group2

---
+++ Lab9.java
@@ -0,0 +1,48 @@
+/* 
+   Co sei 290
+   JinHong Cen
+
+
+
+(Palindrome integer) Write the methods with the following headers
+// Return the reversal of an integer, i.e., reverse(456) returns 654
+public static int reverse(int number)
+// Return true if number is a palindrome
+public static boolean isPalindrome(int number)
+Use the reverse method to implement isPalindrome. A number is a palindrome
+if its reversal is the same as itself. Write a test program that prompts the
+user to enter an integer and reports whether the integer is a palindrome
+*/
+import java.util.*;
+public class Lab9{
+  public static void main(String[] args){
+   
+	 Scanner input = new Scanner(System.in); 
+
+	 
+	 System.out.print("Enter an integer: ");
+   int number = input.nextInt();
+
+	 // Report whether the integer is a palindrome.
+	 System.out.println(number + (isPalindrome(number) ? " is " : " is not ") +
+	 "a palindrome.");
+  }
+  public static int reverse(int number){
+    String reverse = "";  // Holds reversed number
+	  String n = number + ""; // Convert number to string
+	  // Reverse string
+    System.out.println(n);
+    for (int i = n.length() - 1; i >= 0; i--) {
+	  reverse += n.charAt(i);
+	 }
+	 return Integer.parseInt(reverse); // Return reversed integer
+  }
+  
+  public static boolean isPalindrome(int number) {
+	 return number == reverse(number) ? true : false;
+	}
+  
+  
+  
+  
+ }
